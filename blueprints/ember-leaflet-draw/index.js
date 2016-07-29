b96dbeaf183c75cff0122d405b9b98c5debdffd0
Alexander Smith <alex@mapzen.com>
2016-07-29 14:49:56 -0700

    updating dependencies

--- blueprints/ember-leaflet-draw/index.js
+++ blueprints/ember-leaflet-draw/index.js
@@ -7,6 +7,6 @@ module.exports = {
   },
 
   afterInstall: function(options) {
-    return this.addBowerPackageToProject('leaflet-draw', 'https://github.com/Leaflet/Leaflet.draw.git#*');
+    return this.addBowerPackageToProject('leaflet-draw', 'https://github.com/Leaflet/Leaflet.draw.git#0.2.4-dev*');
   }
 };
